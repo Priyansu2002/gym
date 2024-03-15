bdab3d50ccb406acf174dab01f3d7926d1e91e96
Krishna <nayak100krishna@gmail.com>
2024-03-15 11:41:39 +0530

    exercise selector done

---
+++ src/components/ModalBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+const ModalBox = ({ children, open, handleClose }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <Box sx={{ ...style, width: 300 }}>{children}</Box>
+    </Modal>
+  );
+};
+
+export default ModalBox;
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,7 +1,76 @@
 import React from "react";
 
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import ExercisesModal from "../components/ExercisesModal";
+
 const Todo = () => {
-  return <div>Todo</div>;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  function createData(name, exercise, time) {
+    return { name, exercise, time };
+  }
+  const rows = [
+    createData("Monday", ["exercise", "part"], "08:00:00"),
+    createData("Tuesday", ["exercise", "part"], "08:00:00"),
+  ];
+
+  // const handleDelete = (id) => {
+  //   console.info("You clicked the delete icon.", id);
+  // };
+  return (
+    <div>
+      Todo
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Days</TableCell>
+              <TableCell align="right">Exercise</TableCell>
+              <TableCell align="right">Time</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, idx) => (
+              <TableRow
+                key={idx}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">
+                  <div>
+                    {row.exercise.map((exercise, id) => (
+                      <Chip key={id} label={exercise} />
+                    ))}
+                    <Button variant="outlined" onClick={handleOpen}>
+                      Add
+                    </Button>
+                  </div>
+                </TableCell>
+                <TableCell align="right">
+                  <span>{row.time}</span> <input type="time" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <ExercisesModal {...{ open, handleClose }} />
+    </div>
+  );
 };
 
 export default Todo;
